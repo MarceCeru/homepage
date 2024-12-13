9bce9c23cf1a7250c1d25a51df5ca098b5a911e1
Cerutti Marcelo <134024715+MarceCeru@users.noreply.github.com>
2024-12-13 17:01:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: 'https://MarceCeru.github.io/Homepage'
+  build: {
+       mime: {
+         '.jsx': 'application/javascript',
+           },
+       },
 })
